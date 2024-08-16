304c17796fbca0c56aa5e8c4007e8af547604baf
LE Quoc Dat <quocdat.le.insacvl@gmail.com>
2024-08-16 19:25:29 +0200

    fix: Add optional app parameter to get_recent_pdfs() function

--- utils.py
+++ utils.py
@@ -2,13 +2,15 @@ import os
 from datetime import datetime
 from flask import current_app
 
-def get_recent_pdfs():
-    if not os.path.exists(current_app.config['UPLOAD_FOLDER']):
-        os.makedirs(current_app.config['UPLOAD_FOLDER'])
-    files = os.listdir(current_app.config['UPLOAD_FOLDER'])
+def get_recent_pdfs(app=None):
+    if app is None:
+        app = current_app
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
     pdf_files = [f for f in files if f.endswith('.pdf')]
-    pdf_files.sort(key=lambda x: os.path.getmtime(os.path.join(current_app.config['UPLOAD_FOLDER'], x)), reverse=True)
-    return [{'filename': pdf, 'date': datetime.fromtimestamp(os.path.getmtime(os.path.join(current_app.config['UPLOAD_FOLDER'], pdf))).isoformat()} for pdf in pdf_files[:5]]
+    pdf_files.sort(key=lambda x: os.path.getmtime(os.path.join(app.config['UPLOAD_FOLDER'], x)), reverse=True)
+    return [{'filename': pdf, 'date': datetime.fromtimestamp(os.path.getmtime(os.path.join(app.config['UPLOAD_FOLDER'], pdf))).isoformat()} for pdf in pdf_files[:5]]
 
 def parse_language_content(content):
     lines = content.split('\n')
