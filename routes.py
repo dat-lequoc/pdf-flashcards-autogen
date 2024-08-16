8800fad8427cc2a7d5b619d94892bd71ac899cde
LE Quoc Dat <quocdat.le.insacvl@gmail.com>
2024-08-16 19:17:28 +0200

    refactor: Move utility functions to utils.py

--- routes.py
+++ routes.py
@@ -5,11 +5,11 @@ import os
 import json
 from datetime import datetime
 from app import app
-from utils import get_recent_pdfs
+from utils import get_recent_pdfs, parse_language_content, parse_flashcard_content
 
 @app.route('/')
 def index():
-    recent_pdfs = get_recent_pdfs()
+    recent_pdfs = get_recent_pdfs(app.config['UPLOAD_FOLDER'])
     return render_template('index.html', recent_pdfs=recent_pdfs)
 
 @app.route('/upload_pdf', methods=['POST'])
@@ -69,41 +69,4 @@ def generate_flashcard():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-def parse_language_content(content):
-    lines = content.split('\n')
-    word = ''
-    translation = ''
-    answer = ''
-    for line in lines:
-        if line.startswith('T:'):
-            translation = line[2:].strip()
-        elif line.startswith('Q:'):
-            word = line[2:].split('<b>')[1].split('</b>')[0].strip()
-        elif line.startswith('A:'):
-            answer = line[2:].strip()
-    
-    return {
-        'word': word,
-        'question': lines[1][2:].strip(),
-        'translation': translation,
-        'answer': answer
-    }
-
-def parse_flashcard_content(content):
-    flashcards = []
-    current_question = ''
-    current_answer = ''
-
-    for line in content.split('\n'):
-        if line.startswith('Q:'):
-            if current_question and current_answer:
-                flashcards.append({'question': current_question, 'answer': current_answer})
-            current_question = line[2:].strip()
-            current_answer = ''
-        elif line.startswith('A:'):
-            current_answer = line[2:].strip()
-
-    if current_question and current_answer:
-        flashcards.append({'question': current_question, 'answer': current_answer})
-
-    return flashcards
+from utils import parse_language_content, parse_flashcard_content
--- utils.py
+++ utils.py
@@ -48,3 +48,41 @@ def parse_flashcard_content(content):
         flashcards.append({'question': current_question, 'answer': current_answer})
 
     return flashcards
+def parse_language_content(content):
+    lines = content.split('\n')
+    word = ''
+    translation = ''
+    answer = ''
+    for line in lines:
+        if line.startswith('T:'):
+            translation = line[2:].strip()
+        elif line.startswith('Q:'):
+            word = line[2:].split('<b>')[1].split('</b>')[0].strip()
+        elif line.startswith('A:'):
+            answer = line[2:].strip()
+    
+    return {
+        'word': word,
+        'question': lines[1][2:].strip(),
+        'translation': translation,
+        'answer': answer
+    }
+
+def parse_flashcard_content(content):
+    flashcards = []
+    current_question = ''
+    current_answer = ''
+
+    for line in content.split('\n'):
+        if line.startswith('Q:'):
+            if current_question and current_answer:
+                flashcards.append({'question': current_question, 'answer': current_answer})
+            current_question = line[2:].strip()
+            current_answer = ''
+        elif line.startswith('A:'):
+            current_answer = line[2:].strip()
+
+    if current_question and current_answer:
+        flashcards.append({'question': current_question, 'answer': current_answer})
+
+    return flashcards
